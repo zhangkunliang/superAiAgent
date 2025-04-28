61f600cc16f155580be1fca9e59acb98a5fa1a0a
zkl <zhangkunliang@twxszkj.com>
2025-04-28 17:40:34 +0800

    大模型接入、多模态开发

---
+++ src/main/java/com/zkl/zklaiagent/ZklAiAgentApplication.java
@@ -0,0 +1,13 @@
+package com.zkl.zklaiagent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZklAiAgentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZklAiAgentApplication.class, args);
+    }
+
+}
